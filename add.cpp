14e6eeb370afa8285916c3b2810e356caf8d8f4d
Soumya Ranjan Sahoo <48331344+soumyaranjan-01@users.noreply.github.com>
2020-10-23 15:03:16 +0530

    Update add.cpp

--- add.cpp
+++ add.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 
 int add(int a,int b,int c){
-    return a+b+c;
+    return a+b+c+c;
 }
 
 int main(){
     int x,y,z;
     cin >> x >> y >> z;
-    cout << x << "+" << y << "+" << z "=" << add(x,y,z);
+    cout << x << "+" << y << "+" << z " << "+" << z "=" << add(x,y,z);
     return 0;
 }
